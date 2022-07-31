7d56cf9000e748e5a067bfa98b8e46f73f17bc17
VishnuKV9933 <109938148+VishnuKV9933@users.noreply.github.com>
2022-07-31 08:04:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+danger.style.display = 'none';
+success.style.display = 'none';
+noemail.style.display = 'none';
+noname.style.display = 'none';
+function message(){
+    var Name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var msg = document.getElementById('msg');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+    const noemail = document.getElementById('noemail');
+    const noname = document.getElementById('noname');
+    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    let rege = /^([a-zA-Z]){2,20}$/;
+    let s = email.value;
+    let n = Name.value;
+   
+    if(Name.value === '' || email.value === '' ||  msg.value === ''){
+        danger.style.display = 'block';
+    }
+    else if(rege.test(n) && regex.test(s)){
+        setTimeout(() => {
+            Name.value = '';
+            email.value = '';
+            msg.value = '';
+        }, 2000);
+        
+        success.style.display = 'block';
+    } else {
+        if(rege.test(n) == false ){
+            noname.style.display = 'block';
+        }
+        if(regex.test(s) == false){
+            noemail.style.display = 'block';
+        }
+        } 
+        setTimeout(() => {
+            danger.style.display = 'none';
+            success.style.display = 'none';
+            noemail.style.display = 'none';
+            noname.style.display = 'none';
+        }, 4000);
+
+    }
